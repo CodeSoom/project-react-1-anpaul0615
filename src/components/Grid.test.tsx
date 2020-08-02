0071ee91fa2fcb4101b9d135db5fc5e939b31446
Paul An <anpaul0615@gmail.com>
2020-08-03 00:07:22 +0900

    components/Grid 테스트 코드에 그리드 옵션 fixture 적용

--- src/components/Grid.test.tsx
+++ src/components/Grid.test.tsx
@@ -6,36 +6,15 @@ import Grid from './Grid';
 
 import { GridOption } from '../store/types/grid';
 
+import GRID_OPTION from '../store/__fixtures__/grid-option';
+
 describe('Grid', () => {
   const renderComponent = (option: GridOption) => render((
     <Grid option={option} />
   ));
 
   describe('with option', () => {
-    const option: GridOption = {
-      data: [
-        { H1: 'test header 1', H2: 'test header 2', V1: 3, },
-        { H1: 'test header 11', H2: 'test header 22', V1: 33, },
-        { H1: 'test header 111', H2: 'test header 222', V1: 333, },
-      ],
-      columns: [
-        {
-          id: 'H1',
-          name: 'HEADER 1',
-          valueType: typeof String,
-        },
-        {
-          id: 'H2',
-          name: 'HEADER 2',
-          valueType: typeof String,
-        },
-        {
-          id: 'V1',
-          name: 'VALUE 1',
-          valueType: typeof Number,
-        },
-      ],
-    };
+    const option: GridOption = GRID_OPTION;
 
     it('renders grid with option', () => {
       const { getByText } = renderComponent(option);
